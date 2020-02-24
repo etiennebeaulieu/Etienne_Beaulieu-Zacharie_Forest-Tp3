75f33b562a84d4d9362e42eb7c1d623f0209d7a5
zacharieforest <zacharie_forest@icloud.com>
2020-02-24 13:52:53 -0500

    hey

--- src/jeu/JeuMemoire.java
+++ src/jeu/JeuMemoire.java
@@ -1,6 +1,57 @@
 package jeu;
 
-public class JeuMemoire
-{
+import java.awt.Point;
+import java.util.ArrayList;
 
+import formes.*;
+
+public class JeuMemoire implements Memorisable
+{
+	public static final int LIGNE = 6;
+	public static final int COLONNE = 6;
+	public static final int NBR_ELEMENTS_GRILLE = LIGNE*COLONNE;
+	public static final int LONGUEUR_CHAINE = 17;
+	private ArrayList<Point> vecteurPoints = null;
+	private int niveau = 0;
+	private VecteurForme vecteurFormes = null;
+	private Forme grilleDeJeu = null;
+	
+	
+	public JeuMemoire()
+	{
+		
+	}
+	
+	private void preparerVecteurFormes()
+	{
+		
+	}
+	
+	private void preparerGrilleDeJeu()
+	{
+		
+	}
+	
+	public String toString()
+	{
+		return;
+	}
+	
+	private String ajouterEspaces(int LONGUEUR_CHAINE, String pChaine)
+	{
+		
+	}
+	
+	public String getNomForme(int x, int y)
+	{
+		
+	}
+	
+	public ArrayList<Point> jouerOrdi()
+	{
+		
+	}
+	
+	
+	
 }
